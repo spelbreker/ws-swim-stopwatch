d884e7693f41cd7dec7237a0ceaedd0732c0c591
Patrick Brouwer <patrick@333travel.nl>
2025-01-21 15:47:34 +0100

    Add competition data retrieval and athlete entries filtering functionality

--- server.js
+++ server.js
@@ -23,13 +23,12 @@ app.post('/upload', upload.single('lenexFile'), (req, res) => {
         //write result to file
         fs.writeFileSync('./public/competition.json', JSON.stringify(result));
 
-
-        //extract events first session
-        //data that i need swimstyle heats
         let events = result.meets[0].sessions[0].events;
 
         let eventsMap = events.map((event) => {
             return {
+                number: event.number,
+                order: event.order,
                 eventid: event.eventid,
                 gender: event.gender,
                 swimstyle: event.swimstyle,
@@ -44,6 +43,7 @@ app.post('/upload', upload.single('lenexFile'), (req, res) => {
             };
         });
 
+        //write for debugging
         fs.writeFileSync('./public/events.json', JSON.stringify(eventsMap));
 
         let athleatesMap = result.meets[0].clubs.map((club) => {
@@ -65,17 +65,92 @@ app.post('/upload', upload.single('lenexFile'), (req, res) => {
                 };
             });
         });
-
-        console.log('athleatesMap', athleatesMap);
-
+        //write for debugging
         fs.writeFileSync('./public/athleats.json', JSON.stringify(athleatesMap));
-
 
         //add json header to response
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify(result));
     });
 });
+
+app.get('/competition', (req, res) => {
+    //get get param event number and heat number cast to int
+    let eventNumber = parseInt(req.query.event);
+    let heatNumber = parseInt(req.query.heat);
+
+    //if no event number or heat number return error
+    if (!eventNumber || !heatNumber) {
+        res.status(400).send('Missing eventNumber or heatNumber');
+        return;
+    }
+
+    //check if json exist
+    if (!fs.existsSync('./public/competition.json')) {
+        res.status(500).send('Missing competition.json');
+        return;
+    }
+
+    //give eventdata with heat entry data
+    let competitionData = fs.readFileSync('./public/competition.json');
+    competitionData = JSON.parse(competitionData);
+
+    let event = competitionData.meets[0].sessions[0].events.find((event) => event.number === eventNumber);
+    let heat = event.heats.find((heat) => heat.number === heatNumber);
+
+    let competition = {
+        event: {
+            number: event.number,
+            order: event.order,
+            eventid: event.event,
+            swimstyle: event.swimstyle,
+        },
+        heat: heat,
+        entries: findAtlheates(competitionData,event.eventid, heat.heatid),
+    };
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(competition));
+
+});
+
+function findAtlheates(competitionData, event, heat) {
+    console.log('Using eventId:', event, 'and heatId:', heat);
+
+    let entries = competitionData.meets[0].clubs
+        .map((club) => {
+            console.log('Checking club:', club.name);
+            return club.athletes
+                .map((athlete) => {
+                    let filterResult = athlete.entries.filter((entry) => {
+                        return entry.heatid === heat && entry.eventid === event;
+                    });
+                    
+                    if (filterResult.length === 0) {
+                        return null;
+                    }
+                    
+                    return {
+                        athleteid: athlete.athleteid,
+                        firstname: athlete.firstname,
+                        lastname: athlete.lastname,
+                        club: club.name,
+                        lane: filterResult[0]?.lane,
+                        entrytime: filterResult[0]?.entrytime,
+                    };
+                })
+                .filter(Boolean);
+        })
+        .filter((clubEntries) => clubEntries.length > 0);
+
+    //order by lane nr
+    entries = entries.sort((a, b) => {
+        return a[0].lane - b[0].lane;
+    });
+
+    console.log('entries:', entries);
+    return entries;
+}
 
 app.get('*', (req, res) => {
     let filePath = '.' + req.url;
